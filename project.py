069ab9f6305ea8822c04b604845a4ebcb4438736
Marcquez Tookes <marcquez@Marcquezs-MacBook-Pro.local>
2025-03-04 14:01:20 -0600

    Completed Project

--- project.py
+++ project.py
@@ -1,3 +1,11 @@
 print('I am showing you how to push assignments up to GitHub!')
 
 print('Making Changes!')
+grades = int(input("Enter your grade"))
+
+if grades < 70:
+    print("Needs Improvement")
+elif grades >= 70 and grades < 90:
+    print("Good")
+else:
+    print("Excellent")
